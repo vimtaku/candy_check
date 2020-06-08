12b30a888a02f32c33c69d2bad14457125b0a9f1
vimtaku <vimtaku@bestpresent.jp>
2020-06-08 17:42:20 +0900

    Fix return receipts order

--- lib/candy_check/app_store/receipt_collection.rb
+++ lib/candy_check/app_store/receipt_collection.rb
@@ -22,19 +22,23 @@ module CandyCheck
       # Check if in trial
       # @return [bool]
       def trial?
-        @receipts.last.is_trial_period
+        latest_receipt.is_trial_period
       end
 
       # Get latest expiration date
       # @return [DateTime]
       def expires_at
-        @receipts.last.expires_date
+        latest_receipt.expires_date
       end
 
       # Get number of overdue days. If this is negative, it is not overdue.
       # @return [Integer]
       def overdue_days
         (Date.today - expires_at.to_date).to_i
+      end
+
+      def latest_receipt
+        @receipts.sort_by{|x| x.expires_date }.reverse.first
       end
     end
   end
